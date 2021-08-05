a2e024dbff68388a80f361eebf33c35852efa038
shelat <shelat@google.com>
2021-08-05 16:04:44 +0000

    adding run flags

--- run.sh
+++ run.sh
@@ -2,7 +2,9 @@
 
 pwd=`pwd`
 
-docker run -it  \
+docker run  \
+    -d --restart=unless-stopped -p 1812-1813:1812-1813/udp \
+    --restart=unless_stopped \
     -v $pwd/cert.pem:/etc/raddb/cert.pem \
     -v $pwd/key.pem:/etc/raddb/key.pem \
     frrsp
