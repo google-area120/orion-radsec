cfa604802ccec03dd5fdeedbd8136c2e609390da
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2023-12-30 08:00:20 -0600

    Add Cleanup Steps to Docker Setup Script
    Enhanced the Docker setup script to include cleanup steps for removing the old running container and image before building and setting up the new one. This ensures a clean environment with each execution of the script. The script now stops and removes the existing container, followed by the removal of the existing image. Subsequently, it proceeds to build the new image and run the new container. This modification enhances the script's reliability and maintainability.

--- run.sh
+++ run.sh
@@ -5,8 +5,17 @@ CONTAINER_NAME="radsecproxy"
 CERT_PATH=$(pwd)/cert.pem
 KEY_PATH=$(pwd)/key.pem
 
+# Stop and remove the existing container (if it exists)
+sudo docker stop $CONTAINER_NAME
+sudo docker rm $CONTAINER_NAME
+
+# Remove the existing image (if it exists)
+sudo docker rmi $IMAGE_NAME
+
+# Build the new image
 sudo docker build -t $IMAGE_NAME .
 
+# Run the new container
 sudo docker run  \
     -d --restart=unless-stopped \
     --network host \
