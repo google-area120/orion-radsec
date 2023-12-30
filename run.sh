b7a025ae2767611fbbce87b0b73e2d5e778e35d2
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2023-12-30 07:46:10 -0600

    Refactor Run Script for Improved Readability and Flexibility
    Updated the Docker run script to enhance readability and provide flexibility. Introduced variables for image and container names, and used them consistently throughout the script. Adopted environment variables for certificate and key paths. These changes improve maintainability and make it easier to customize the script for different environments.

--- run.sh
+++ run.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-pwd=`pwd`
+IMAGE_NAME="frrsp"
+CONTAINER_NAME="radsecproxy"
+CERT_PATH=$(pwd)/cert.pem
+KEY_PATH=$(pwd)/key.pem
 
-sudo docker build -t frrsp .
+sudo docker build -t $IMAGE_NAME .
 
 sudo docker run  \
     -d --restart=unless-stopped \
+    --network host \
     -p 1812-1813:1812-1813/udp \
-    -v $pwd/cert.pem:/etc/raddb/cert.pem \
-    -v $pwd/key.pem:/etc/raddb/key.pem \
-    frrsp
-
+    -v $CERT_PATH:/etc/raddb/cert.pem \
+    -v $KEY_PATH:/etc/raddb/key.pem \
+    --name $CONTAINER_NAME \
+    $IMAGE_NAME
